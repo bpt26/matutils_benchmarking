c9e80040d19d80882f8075fffbc03b1031636471
Bryan Thornlow <bthornlo@ucsc.edu>
2021-06-14 21:03:24 -0700

    Add files via upload

--- KEY_SCRIPTS/apeResolve.R
+++ KEY_SCRIPTS/apeResolve.R
@@ -1,5 +1,4 @@
 library(ape)
-t1=read.tree("public-2021-05-24.all.masked.nextclade.pangolin.nwk")
+t1=read.tree("public-2021-06-09.all.masked.nextclade.pangolin.nwk")
 t1b<-multi2di(t1)
-write.tree(t1b,file="public-2021-05-24.all.masked.nextclade.pangolin.resolved.nwk")
-
+write.tree(t1b,file="public-2021-06-09.all.masked.nextclade.pangolin.resolved.nwk")
